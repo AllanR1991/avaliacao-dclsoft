29e180143b09d9f005d96e97d92e040eddd00c5a
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-04-15 14:03:09 -0300

    Adicionado barra de carregamento no programa e modifica a exibição do resultado

--- exercicio1/Program.cs
+++ exercicio1/Program.cs
@@ -4,10 +4,17 @@ using exercicio1;
 int anos, meses, dias, res;
 char novoCalculo;
 
-PeR.ExibeMensagemPulandoLinha("\n\nEste programa exibe a idade de uma pessoa expressa apenas em dias.\n");
+Console.Clear();
+
+Calc.BarraCarregamento("Carregando o sistema",10,300);
+
+Console.Clear();
+
+PeR.ExibeMensagemPulandoLinha("Este programa exibe a idade de uma pessoa expressa apenas em dias.");
 
 do{
-    PeR.ExibeMensagemPulandoLinha("Informe a idade expressa em anos, meses e dias.\n");
+    
+    PeR.ExibeMensagemPulandoLinha("\nInforme a idade expressa em anos, meses e dias.");
 
     anos = PeR.PerguntaInt("Informe o(s) ano(s): ");
     meses = PeR.PerguntaInt("Informe o(s) mês(es)");
@@ -15,12 +22,23 @@ do{
 
     res = Calc.ConvertIdadeEmDias(anos,meses,dias);
 
-    PeR.ExibeMensagemPulandoLinha($"\n\nA idade expressa em {anos} ano(s), {meses} mês(es) e {dias} dia(s), convertida em apenas dias é de : {res} dia(s)\n\n");
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    PeR.ExibeMensagemPulandoLinha(@$"
+***************************************************************************************************************************
 
+        A idade expressa em {anos} ano(s), {meses} mês(es) e {dias} dia(s), convertida em apenas dias é de : {res} dia(s)     
+
+***************************************************************************************************************************    
+
+    ");
+    Console.ResetColor();
+    
     novoCalculo = PeR.PerguntaChar("Gostaria de fazer um novo calculo, (s) para sim e (n) para não : ");
 
 }while(novoCalculo != 'n');
 
-PeR.ExibeMensagemPulandoLinha("Fim do programa.");
+Calc.BarraCarregamento("Fechando o sistema",10,300);
+
+PeR.ExibeMensagemPulandoLinha("\n\nSistema finalziado com sucesso.");
 
 
