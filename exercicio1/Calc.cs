f600a9f0d3e8be5beefadb64bfae2b387f626458
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-04-15 14:02:12 -0300

    Adicionado metodo de carregamento

--- exercicio1/Calc.cs
+++ exercicio1/Calc.cs
@@ -2,11 +2,22 @@ namespace exercicio1
 {
     public class Calc
     {
-        public static int ConvertIdadeEmDias(int anos,int meses,int dias){
-            int anosEmDias = anos*365;
-            int mesesEmDias= meses*30;
+        public static int ConvertIdadeEmDias(int anos, int meses, int dias)
+        {
+            int anosEmDias = anos * 365;
+            int mesesEmDias = meses * 30;
 
-            return anosEmDias+mesesEmDias+dias;
+            return anosEmDias + mesesEmDias + dias;
+        }
+
+        public static void BarraCarregamento(string texto, int quantidadePontinhos, int tempo)
+        {
+            PeR.ExibeMensagem(texto);
+            for (int i = 0; i <= quantidadePontinhos; i++)
+            {
+                PeR.ExibeMensagem(".");
+                Thread.Sleep(tempo);
+            }
         }
     }
 }
