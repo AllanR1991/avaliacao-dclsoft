35623fe282dfc4deec73b86b6dda22a6f31cb670
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-04-15 18:32:16 -0300

    Alterado a Classe dos medotos.

--- exercicio1/Program.cs
+++ exercicio1/Program.cs
@@ -6,24 +6,24 @@ char novoCalculo;
 
 Console.Clear();
 
-Calc.BarraCarregamento("Carregando o sistema",10,300);
+Utils.BarraCarregamento("Carregando o sistema",10,300);
 
 Console.Clear();
 
-PeR.ExibeMensagemPulandoLinha("Este programa exibe a idade de uma pessoa expressa apenas em dias.");
+Utils.ExibeMensagemPulandoLinha("Este programa exibe a idade de uma pessoa expressa apenas em dias.");
 
 do{
     
-    PeR.ExibeMensagemPulandoLinha("\nInforme a idade expressa em anos, meses e dias.");
+    Utils.ExibeMensagemPulandoLinha("\nInforme a idade expressa em anos, meses e dias.");
 
-    anos = PeR.PerguntaInt("Informe o(s) ano(s): ");
-    meses = PeR.PerguntaInt("Informe o(s) mês(es)");
-    dias = PeR.PerguntaInt("Informe o(s) dia(s)");
+    anos = Utils.PerguntaInt("Informe os anos : ");
+    meses = Utils.PerguntaInt("Informe os meses : ");
+    dias = Utils.PerguntaInt("Informe os dia : ");
 
     res = Calc.ConvertIdadeEmDias(anos,meses,dias);
 
     Console.ForegroundColor = ConsoleColor.Yellow;
-    PeR.ExibeMensagemPulandoLinha(@$"
+    Utils.ExibeMensagemPulandoLinha(@$"
 ***************************************************************************************************************************
 
         A idade expressa em {anos} ano(s), {meses} mês(es) e {dias} dia(s), convertida em apenas dias é de : {res} dia(s)     
@@ -33,12 +33,11 @@ do{
     ");
     Console.ResetColor();
     
-    novoCalculo = PeR.PerguntaChar("Gostaria de fazer um novo calculo, (s) para sim e (n) para não : ");
-
+    novoCalculo = Utils.PerguntaChar("Gostaria de fazer um novo calculo, (s) para sim e (n) para não : ");
 }while(novoCalculo != 'n');
 
-Calc.BarraCarregamento("Fechando o sistema",10,300);
+Utils.BarraCarregamento("\nFechando o sistema",10,300);
 
-PeR.ExibeMensagemPulandoLinha("\n\nSistema finalziado com sucesso.");
+Utils.ExibeMensagemPulandoLinha("\n\nSistema finalziado com sucesso.");
 
 
