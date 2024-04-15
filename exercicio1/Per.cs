e4068c494a33790a7de3994b3bce3de767701abe
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-04-15 13:35:05 -0300

    Realizado alteração para validações de inteiros, e pontos flutuantes

--- exercicio1/Per.cs
+++ exercicio1/Per.cs
@@ -4,26 +4,104 @@ namespace exercicio1
     {
         public static string PerguntaString(string pergunta)
         {
-            Console.WriteLine(pergunta);
+            Console.WriteLine($"\n{pergunta}");
             return Console.ReadLine();
         }
 
         public static char PerguntaChar(string pergunta)
         {
-            Console.WriteLine(pergunta);
-            return char.Parse(Console.ReadLine());
+           bool check=true; 
+           char response;
+            do{
+                if(check){
+                    Console.WriteLine($"\n{pergunta}");
+                    check = char.TryParse(Console.ReadLine(), out response);                
+                }else{
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("O valor inserido é invalido, favor inserir um numero inteiro: ");
+                    Console.ResetColor();
+                    check = char.TryParse(Console.ReadLine(), out response);
+                }
+                Console.ResetColor();
+            }while(!check);
+            return response;
         }
+
         public static int PerguntaInt(string pergunta)
         {
-            Console.WriteLine(pergunta);
-            return int.Parse(Console.ReadLine());
+           bool check=true; 
+           int response;
+            do{
+                if(check){
+                    Console.WriteLine($"\n{pergunta}");
+                    check = int.TryParse(Console.ReadLine(), out response);                
+                }else{
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("O valor inserido é invalido, favor inserir um numero inteiro: ");
+                    Console.ResetColor();
+                    check = int.TryParse(Console.ReadLine(), out response);
+                }
+                Console.ResetColor();
+            }while(!check);
+            return response;
         }
 
         public static float PerguntaFloat(string pergunta)
         {
-            Console.WriteLine(pergunta);
-            return float.Parse(Console.ReadLine());
+            bool check=true; 
+            float response;
+            do{
+                if(check){
+                    Console.WriteLine($"\n{pergunta}");
+                    check = float.TryParse(Console.ReadLine(), out response);                
+                }else{
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("O valor inserido é invalido, favor inserir um numero inteiro: ");
+                    Console.ResetColor();
+                    check = float.TryParse(Console.ReadLine(), out response);
+                }
+                Console.ResetColor();
+            }while(!check);
+            return response;
         }
+
+        public static double PerguntaDouble(string pergunta)
+        {
+            bool check=true; 
+            double response;
+            do{
+                if(check){
+                    Console.WriteLine($"\n{pergunta}");
+                    check = double.TryParse(Console.ReadLine(), out response);                
+                }else{
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("O valor inserido é invalido, favor inserir um numero inteiro: ");
+                    Console.ResetColor();
+                    check = double.TryParse(Console.ReadLine(), out response);
+                }
+                Console.ResetColor();
+            }while(!check);
+            return response;
+        }
+
+        public static decimal PerguntaDecimal(string pergunta)
+        {
+            bool check=true; 
+            decimal response;
+            do{
+                if(check){
+                    Console.WriteLine($"\n{pergunta}");
+                    check = decimal.TryParse(Console.ReadLine(), out response);                
+                }else{
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("O valor inserido é invalido, favor inserir um numero inteiro: ");
+                    Console.ResetColor();
+                    check = decimal.TryParse(Console.ReadLine(), out response);
+                }
+                Console.ResetColor();
+            }while(!check);
+            return response;
+        }        
 
         public static void ExibeMensagemPulandoLinha(string texto)
         {
