420a6b3ce177c6745c53680e571613b092f25c3f
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-04-15 13:36:37 -0300

    Criado a estrutura do progrma

--- exercicio1/Program.cs
+++ exercicio1/Program.cs
@@ -1,1 +1,26 @@
 ﻿
+using exercicio1;
+
+int anos, meses, dias, res;
+char novoCalculo;
+
+PeR.ExibeMensagemPulandoLinha("\n\nEste programa exibe a idade de uma pessoa expressa apenas em dias.\n");
+
+do{
+    PeR.ExibeMensagemPulandoLinha("Informe a idade expressa em anos, meses e dias.\n");
+
+    anos = PeR.PerguntaInt("Informe o(s) ano(s): ");
+    meses = PeR.PerguntaInt("Informe o(s) mês(es)");
+    dias = PeR.PerguntaInt("Informe o(s) dia(s)");
+
+    res = Calc.ConvertIdadeEmDias(anos,meses,dias);
+
+    PeR.ExibeMensagemPulandoLinha($"\n\nA idade expressa em {anos} ano(s), {meses} mês(es) e {dias} dia(s), convertida em apenas dias é de : {res} dia(s)\n\n");
+
+    novoCalculo = PeR.PerguntaChar("Gostaria de fazer um novo calculo, (s) para sim e (n) para não : ");
+
+}while(novoCalculo != 'n');
+
+PeR.ExibeMensagemPulandoLinha("Fim do programa.");
+
+
