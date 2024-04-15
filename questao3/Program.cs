f8e4aa0793442baf9b5e876aaf3639b1df74dfcc
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2024-04-15 17:41:00 -0300

    Criado a estrutura do programa

---
+++ questao3/Program.cs
@@ -0,0 +1,41 @@
+﻿using questao3;
+
+Console.Clear();
+Utils.BarraCarregamento("\n\n\nIniciando o programa", 5,500);
+Console.Clear();
+float   comissaoCarroVendido,
+        salarioFixo,
+        valorVendas;
+
+int qtdCarroVendido;
+
+Utils.ExibeMensagemPulandoLinha("Este programa calcula o salário de um vendedor de carro.\n\n");
+
+qtdCarroVendido = Utils.PerguntaInt("Informe a quantidade de carros vendidos pelo vendedor: ");
+valorVendas = Utils.PerguntaFloat("Informe o valor total das vendas realizadas pelo vendedor: ");
+salarioFixo = Utils.PerguntaFloat("Informe o salário fixo do vendedor: ");
+comissaoCarroVendido = Utils.PerguntaFloat("Informe a comissão fixa que o vendedor ganha por carro vendido: ");
+
+Calc.CalculaSalario(qtdCarroVendido,valorVendas,salarioFixo,comissaoCarroVendido);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
